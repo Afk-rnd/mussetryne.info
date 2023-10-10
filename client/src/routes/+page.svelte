ce5cb080cfc4ecf552a82fd0af153090890567d6
Morten Hovstad <morten.hovstad@gmail.com>
2023-10-10 17:42:17 +0200

    More work

--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -16,13 +16,12 @@
 <Navbar>
   <NavBrand href="/">
     <img
-      src="/images/flowbite-svelte-icon-logo.svg"
-      class="mr-3 h-6 sm:h-9"
+      src="https://mussetryne.info/resources/logo.png"
+      class="mr-3 h-16 sm:h-22"
       alt="Flowbite Logo"
     />
-    <span
-      class="self-center whitespace-nowrap text-xl font-semibold dark:text-white"
-      >Flowbite</span
+    <span class="self-center text-xl font-semibold dark:text-white"
+      >Opplysningssenteret for<br />Medisinsk <br /> Mussetryne</span
     >
   </NavBrand>
   <div class="flex items-center md:order-2">
@@ -31,17 +30,18 @@
   </div>
   <Dropdown placement="bottom" triggeredBy="#avatar-menu">
     <DropdownHeader>
-      <span class="block text-sm">Bonnie Green</span>
-      <span class="block truncate text-sm font-medium">name@flowbite.com</span>
+      <span class="block text-sm">AFK NORMANN</span>
+      <span class="block truncate text-sm font-medium">afk@mussetryne.info</span
+      >
     </DropdownHeader>
     <DropdownItem>Dashboard</DropdownItem>
     <DropdownItem>Settings</DropdownItem>
-    <DropdownItem>Earnings</DropdownItem>
+    <DropdownItem>Straffefisk</DropdownItem>
     <DropdownDivider />
     <DropdownItem>Sign out</DropdownItem>
   </Dropdown>
   <NavUl>
-    <NavLi href="/" active={true}>Home</NavLi>
+    <NavLi href="/" active={true}>Hjem</NavLi>
     <NavLi href="/about">About</NavLi>
     <NavLi href="/docs/components/navbar">Navbar</NavLi>
     <NavLi href="/pricing">Pricing</NavLi>
