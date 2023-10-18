503ca4718f7a3afb2e35845eb732b358e36c0254
Chris Vaksdal <chris.vaksdal@gmail.com>
2023-10-18 17:05:51 +0200

    Added preview of uploaded file in settings.

--- client/src/components/MussePictureUploadForm.svelte
+++ client/src/components/MussePictureUploadForm.svelte
@@ -1,12 +1,16 @@
 <script>
+    import { ImagePlaceholder } from 'flowbite-svelte';
     import { Label, Fileupload } from 'flowbite-svelte';
     import { authenticatedPostRequest } from '../lib/authenticated';
+
 
     let fileuploadprops = {
         accept: "image/*",
         name: "mussepicture",
+        id: "mussepicture",
     };
 
+    
     async function handleSubmit(event) {
         event.preventDefault();
         const form = event.target;
@@ -14,12 +18,28 @@
         const json = await authenticatedPostRequest("http://localhost:42069/upload_mussepicture", data, "", "form");
     }
 
+    let image = null;
+    
+    function handle_file_upload(event) {
+        const file = event.target.files[0];
+        if(file) {
+            image = URL.createObjectURL(file);
+        }
+    }
+
+
 </script>
 
 <form method="POST" action="localhost:42069/upload_mussepicture" on:submit={handleSubmit}>
     
     <Label class="pb-2">Last opp mussebilde</Label>
-    <Fileupload {...fileuploadprops} />
+    <Fileupload {...fileuploadprops} on:change={handle_file_upload}/>
+
+    {#if image}
+        <img class="mt-8 mb-4" src={image} alt="Ditt mussebilde."/>
+    {:else}
+        <ImagePlaceholder imgHeight={60} class="mt-8"/>
+    {/if}
 
     <button type="submit" class="centered text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700" >Last opp</button>
 
